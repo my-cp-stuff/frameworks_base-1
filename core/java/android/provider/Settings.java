f06f2f76e7845217b3e0a2682b9fd5b32e223ff4
NurKeinNeid <mralexman3000@gmail.com>
2021-01-17 17:03:03 +0100

    Revert "Revert "base: Add navigation bar visibility control [1/2]""
    This reverts commit 3eed69e2ef9460375e69ff3d1143b1b216a40ea5.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5289,6 +5289,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String FORCE_SHOW_NAVBAR = "navbar_visibility";
+
+        /**
+         * @hide
+         */
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
